a038cc6e0f5f441896efe2e7dada81e506989877
graoly <roy.gal@mail.utoronto.ca>
2024-09-12 14:44:18 -0400

    Update FizzBuzz.java

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -1,33 +1,42 @@
 /**
  * Solve the FizzBuzz challenge.
  */
+
 class FizzBuzz {
 
+    private static void func(int i) {
+        boolean divisibleBy3 = i % 3 == 0;
+        boolean divisibleBy5 = i % 5 == 0;
+
+        // Print our appropriate result.
+        if (divisibleBy3 && divisibleBy5) {
+            System.out.println("Fizz Buzz");
+
+        } else if (divisibleBy3) {
+            System.out.println("Fizz");
+
+        } else if (divisibleBy5) {
+            System.out.println("Buzz");
+
+        } else {
+            System.out.println(i);
+        }
+    }
+    private static String condition = "WHILE";
     public static void main(String[] args) {
+        if (condition.equals("IF")) {
+            for (int i = 1; i < 100; i++) {
+                // Find out which numbers divide i.
+                func(i);
+            }
+        }
 
-        for (int i = 1; i < 100; i++) {
-
-            // Find out which numbers divide i.
-            boolean divisibleBy3 = i % 3 == 0;
-            boolean divisibleBy5 = i % 5 == 0;
-
-            // Print our appropriate result.
-            if (divisibleBy3 && divisibleBy5) {
-
-                System.out.println("Fizz Buzz");
-
-            } else if (divisibleBy3) {
-
-                System.out.println("Fizz");
-
-            } else if (divisibleBy5) {
-
-                System.out.println("Buzz");
-
-            } else {
-
-                System.out.println(i);
-
+        else if (condition.equals("WHILE")) {
+            var i = 1;
+            while (i < 100) {
+                // Find out which numbers divide i.
+                func(i);
+                i += 1;
             }
         }
     }
